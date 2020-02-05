6415b9b09d298aa98e5f58ecd0a2e187eb412980
felipeabreu <felipeabreu.rj@gmail.com>
2020-02-05 19:45:28 -0300

    Se aplicado, este commit irá configurar a estrutura inicial do projeto Frases do dia.

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Frases do dia', // Titulo
+    home: Container(color: Colors.white), // Estrutura inicial do app
+  ));
+}
